530743afaa61f8ea1f6d24ba581a8352927b5181
Lore Goethals <lore.goethals2@etu.univ-lorraine.fr>
2025-02-04 20:17:16 +0100

    Reading in file notes and test

--- projet_notes/modele.py
+++ projet_notes/modele.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 
 DEFAULT_LOGS = "logs.csv"
+DEFAULT_NOTES = "notes.csv"
 
 def get_logs(filename = DEFAULT_LOGS):
     '''
@@ -17,6 +18,22 @@ def get_logs(filename = DEFAULT_LOGS):
         logging.error(msg=f"Erreur de chargement du dataset : {filename}")
         sys.exit()
 
+def get_notes(filename = DEFAULT_NOTES):
+    '''
+    Reads in datafile with notes
+    :param filename: file in format csv
+    :return: notes (if file charged)
+    '''
+    try:
+        notes = pd.read_csv(filename)
+        return notes
+    except:
+        logging.error(msg=f"Erreur de chargement du dataset : {filename}")
+        sys.exit()
+
 if __name__ == "__main__":
-    print(get_data().info())
-    print(get_data())
+    print(get_logs().info())
+    print(get_logs())
+
+    print(get_notes().info())
+    print(get_notes())
