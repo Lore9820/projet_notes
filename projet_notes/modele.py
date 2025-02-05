ecab5ed8d42b8a8b3c94038029daf8f7e6de6c3c
Lore Goethals <lore.goethals2@etu.univ-lorraine.fr>
2025-02-05 13:33:17 +0100

    Adding function to filter logs to remove pseudos without correspondance

--- projet_notes/modele.py
+++ projet_notes/modele.py
@@ -31,9 +31,21 @@ def get_notes(filename = DEFAULT_NOTES):
         logging.error(msg=f"Erreur de chargement du dataset : {filename}")
         sys.exit()
 
+def filter_logs(file_logs, file_notes):
+    '''
+    Fonction qui permet d'enlever les lignes dans le DataFrame logs qui n'ont pas de pseudo correspondant dans le DataFrame notes
+    :param file_logs: le fichier avec les logs
+    :param file_notes: le fichier avec les notes
+    :return: le fichier logs filtré (lignes sans correspondance enlevé)
+    '''
+    logs = file_logs[file_logs["pseudo"].isin(file_notes["pseudo"])]
+    return logs
+
 if __name__ == "__main__":
     print(get_logs().info())
     print(get_logs())
 
     print(get_notes().info())
     print(get_notes())
+
+    print(filter_logs(get_logs(), get_notes()).info())
