1af61258d7c6d919d59d0c5aa78c371d3849cd26
Lore Goethals <lore.goethals2@etu.univ-lorraine.fr>
2025-02-05 15:48:50 +0100

    Adding functions for mean and max number of actions per day

--- projet_notes/control.py
+++ projet_notes/control.py
@@ -1,14 +1,33 @@
 import logging
 import pandas as pd
 
-def nb_actions(data:pd.DataFrame):
+def nb_actions(df_logs:pd.DataFrame):
     '''
-    Calculer le nombre d'actions réalisées par chaque apprenant
-    :param data: dataset anonymisé
+    Calculer le nombre d'actions réalisées par chaque personne
+    :param data: dataset anonymisé avec les logs
     :return: DataFrame pseudo -> nb
     '''
-    res = data.groupby('pseudo').size().reset_index(name='nb')
-    res = res.sort_values(by='nb', ascending=False)
+    res = df_logs.groupby('pseudo').size().reset_index(name='nb')
+    return res
+
+def moyenne_actions_par_jour(df_logs:pd.DataFrame):
+    '''
+    Calcule le nombre d'actions par jour
+    :param df_logs: DataFrame anonymisé avec les logs
+    :return:
+    '''
+    res = df_logs.groupby(['pseudo', 'jour']).size().reset_index(name='nb')
+    res = res.groupby('pseudo')['nb'].mean().reset_index(name='moyenne_nb')
+    return res
+
+def max_actions_par_jour(df_logs:pd.DataFrame):
+    '''
+    Calcule le nombre d'actions par jour
+    :param df_logs: DataFrame anonymisé avec les logs
+    :return:
+    '''
+    res = df_logs.groupby(['pseudo', 'jour']).size().reset_index(name='nb')
+    res = res.groupby('pseudo')['nb'].max().reset_index(name='max_nb')
     return res
 
 if __name__ == '__main__':
@@ -16,5 +35,13 @@ if __name__ == '__main__':
 
     #test1
     logs = modele.get_logs()
-    pseudo_count = nb_actions(logs)
-    print(pseudo_count)
+    notes = modele.get_notes()
+    logs = modele.filter_logs(logs, notes)
+    logs = modele.split_columns(logs)
+
+    nb_actions = nb_actions(logs)
+    print(nb_actions)
+
+    print(moyenne_actions_par_jour(logs))
+    print(max_actions_par_jour(logs))
+
