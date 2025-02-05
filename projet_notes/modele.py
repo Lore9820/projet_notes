6d5321c2c7ab3a5bd19afb8b4bdc7cc22028f014
Lore Goethals <lore.goethals2@etu.univ-lorraine.fr>
2025-02-05 15:07:31 +0100

    Adding function split_columns

--- projet_notes/modele.py
+++ projet_notes/modele.py
@@ -9,7 +9,7 @@ def get_logs(filename = DEFAULT_LOGS):
     '''
     Reads in datafile with logs
     :param filename: file in format csv
-    :return: logs (if file charged)
+    :return: DataFrame logs (if file charged)
     '''
     try:
         logs = pd.read_csv(filename, parse_dates=['heure'])
@@ -22,7 +22,7 @@ def get_notes(filename = DEFAULT_NOTES):
     '''
     Reads in datafile with notes
     :param filename: file in format csv
-    :return: notes (if file charged)
+    :return: DataFrame notes (if file charged)
     '''
     try:
         notes = pd.read_csv(filename)
@@ -31,15 +31,28 @@ def get_notes(filename = DEFAULT_NOTES):
         logging.error(msg=f"Erreur de chargement du dataset : {filename}")
         sys.exit()
 
-def filter_logs(file_logs, file_notes):
+def filter_logs(df_logs, df_notes):
     '''
     Fonction qui permet d'enlever les lignes dans le DataFrame logs qui n'ont pas de pseudo correspondant dans le DataFrame notes
-    :param file_logs: le fichier avec les logs
-    :param file_notes: le fichier avec les notes
-    :return: le fichier logs filtré (lignes sans correspondance enlevé)
+    :param df_logs: le fichier avec les logs
+    :param df_notes: le fichier avec les notes
+    :return: le DataFrame logs filtré (lignes sans correspondance enlevé)
     '''
-    logs = file_logs[file_logs["pseudo"].isin(file_notes["pseudo"])]
+    logs = df_logs[df_logs["pseudo"].isin(df_notes["pseudo"])]
     return logs
+
+def split_columns(df_logs):
+    '''
+    Fonction qui permet de séparer le colonne contexte en contexte générale et spécifications,
+    et la colonne heure (datetime) en jour et heures
+    :param df_logs: le DataFrame avec les logs
+    :return: DataFrame logs avec colonnes séparé
+    '''
+    df_logs[["contexte_general", "specification"]] = df_logs["contexte"].str.split(": ", n=1, expand=True)
+    df_logs["jour"] = df_logs["heure"].dt.date
+    df_logs["heures"] = df_logs["heure"].dt.time
+    df_logs = df_logs.drop(["contexte", "heure"], axis=1)
+    return df_logs
 
 if __name__ == "__main__":
     print(get_logs().info())
@@ -49,3 +62,5 @@ if __name__ == "__main__":
     print(get_notes())
 
     print(filter_logs(get_logs(), get_notes()).info())
+
+    print(split_columns(get_logs()))
